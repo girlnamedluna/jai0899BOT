2ad321b7195aa4e000a3634d5fa092cc7a99a5d2
girlnamedluna <mossmossstudios@gmail.com>
2024-05-31 12:19:23 -0400

    Create CommandLIB.cs

---
+++ CommandLIB.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLIB
+{
+    public class TwitchBot
+    {
+        const string ip = "irc.chat.twitch.tv";
+        const int port = 6667;
+
+        private string nick;
+        private string password;
+        private StreamReader streamReader;
+        private StreamWriter streamWriter;
+        private TaskCompletionSource<int> connected = new TaskCompletionSource<int>();
+
+        public event TwitchChatEventHandler OnMessage = delegate { };
+        public delegate void TwitchChatEventHandler(object sender, TwitchChatMessage e);
+
+        public class TwitchChatMessage : EventArgs
+        {
+            public string Sender { get; set; }
+            public string Message { get; set; }
+            public string Channel { get; set; }
+        }
+
+        public TwitchBot(string nick, string password)
+        {
+            this.nick = nick;
+            this.password = password;
+        }
+
+        public async Task Start()
+        {
+            var tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync(ip, port);
+            streamReader = new StreamReader(tcpClient.GetStream());
+            streamWriter = new StreamWriter(tcpClient.GetStream()) { NewLine = "\r\n", AutoFlush = true };
+
+            await streamWriter.WriteLineAsync($"PASS {password}");
+            await streamWriter.WriteLineAsync($"NICK {nick}");
+            connected.SetResult(0);
+
+            while (true)
+            {
+                string line = await streamReader.ReadLineAsync();
+                Console.WriteLine(line);
+
+                string[] split = line.Split(" ");
+                if (line.StartsWith("PING"))
+                {
+                    Console.WriteLine("PING");
+                    await streamWriter.WriteLineAsync($"PONG {split[1]}");
+                }
+                if (split.Length > 1 && split[1] == "PRIVMSG")
+                {
+                    int exclmationPointPosition = split[0].IndexOf("!");
+                    string username = split[0].Substring(1, exclmationPointPosition - 1);
+                    int secondColonPosition = line.IndexOf(':', 1);
+                    string message = line.Substring(secondColonPosition + 1);
+                    string channel = split[2].TrimStart('#');
+
+                    OnMessage(this, new TwitchChatMessage
+                    {
+                        Message = message,
+                        Channel = channel,
+                        Sender = username
+                    });
+                }
+            }
+        }
+        public async Task SendMessage(string channel, string message)
+        {
+            await connected.Task;
+            await streamWriter.WriteLineAsync($"PRIVMSG #{channel} :{message}");
+        }
+        public async Task JoinChannel(string channel)
+        {
+            await connected.Task;
+            await streamWriter.WriteLineAsync($"JOIN #{channel}");
+        }
+    }
+}
